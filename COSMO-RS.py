5b0a061261eb53152a33ee4fca02091e5ad4dda6
ecustZiXinChen <156098829+ecustZiXinChen@users.noreply.github.com>
2024-03-22 22:09:02 +0800

    Update COSMO-RS.py

--- COSMO-RS.py
+++ COSMO-RS.py
@@ -7,7 +7,9 @@ from math import sqrt
 
 df = pd.read_excel('dataILnew.xlsx')
 y_cosmo = pd.DataFrame(df.iloc[:, -1])
+print(y_cosmo)
 yexp = pd.DataFrame(df.iloc[:, -2])
+print(yexp)
 r2 = r2_score(yexp, y_cosmo)
 mae = mean_absolute_error(yexp, y_cosmo)
 print(r2, mae)
